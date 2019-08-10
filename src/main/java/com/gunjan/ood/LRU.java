d2b6465a2d2193f1f1fd5c488ff3de70eba2f636
gunjanvishwakarma <gunjan.it2k4@gmail.com>
2019-08-11 01:09:06 +0530

    LRU using doubly linked list

--- src/main/java/com/gunjan/ood/LRU.java
+++ src/main/java/com/gunjan/ood/LRU.java
@@ -1,34 +1,111 @@
 package com.gunjan.ood;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 
-public class LRU {
-    private int capacity;
-    private LinkedList<Integer> keys = new LinkedList<>();
-    private Map<Integer,Integer> map = new HashMap<>();
-    public LRU(int capacity) {
+import java.util.HashMap;
+
+public class LRU
+{
+    private Node head;
+    private Node tail;
+    private HashMap<Integer,Node> map = null;
+    int capacity;
+    
+    public LRU(int capacity)
+    {
         this.capacity = capacity;
-        keys.clear();
-        map.clear();
+        this.map = new HashMap<>();
     }
     
-    public int get(int key) {
-        Integer value =  map.get(key);
-        if(value != null){
-             keys.remove(keys.indexOf(key));
-             keys.addFirst(key);
+    public int get(int key)
+    {
+        if(map.get(key) == null)
+        {
+            return -1;
         }
-        return value != null ? value : -1;
+        //move to tail
+        Node t = map.get(key);
+        
+        removeNode(t);
+        addNode(t);
+        
+        return t.value;
     }
     
-    public void set(int key, int value) {
-        map.put(key,value);
-        if(keys.indexOf(key) != -1) keys.remove(keys.indexOf(key));
-        keys.addFirst(key);
-        if(map.size() > capacity){
-            Integer last = keys.removeLast();
-            map.remove(last);
+    public void put(int key, int value)
+    {
+        if(map.containsKey(key))
+        {
+            Node t = map.get(key);
+            t.value = value;
+            
+            //move to tail
+            removeNode(t);
+            addNode(t);
         }
+        else
+        {
+            if(map.size() >= capacity)
+            {
+                //delete head
+                map.remove(head.key);
+                removeNode(head);
+            }
+            
+            //add to tail
+            Node node = new Node(key, value);
+            addNode(node);
+            map.put(key, node);
+        }
+    }
+    
+    private void removeNode(Node n)
+    {
+        if(n.prev != null)
+        {
+            n.prev.next = n.next;
+        }
+        else
+        {
+            head = n.next;
+        }
+        
+        if(n.next != null)
+        {
+            n.next.prev = n.prev;
+        }
+        else
+        {
+            tail = n.prev;
+        }
+    }
+    
+    private void addNode(Node n)
+    {
+        if(tail != null)
+        {
+            tail.next = n;
+        }
+        
+        n.prev = tail;
+        n.next = null;
+        tail = n;
+        
+        if(head == null)
+        {
+            head = tail;
+        }
+    }
+}
+
+class Node
+{
+    int key;
+    int value;
+    Node prev;
+    Node next;
+    
+    public Node(int key, int value)
+    {
+        this.key = key;
+        this.value = value;
     }
 }
